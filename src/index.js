2e22f4f3a2a2a059da64910be4d89476cd50177e
abdalrhmanatass <a.atassi2003@gmail.com>
2021-09-29 16:36:55 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./css/style.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "jquery/dist/jquery.min.js";
+import "popper.js/dist/popper.min.js";
+import "@laylazi/bootstrap-rtl/dist/css/bootstrap.min.css";
+console.log("TEST");
