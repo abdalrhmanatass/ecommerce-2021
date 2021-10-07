1232ecaf6ab5a87b5974d1ab48f6db5f3e84065c
abdalrhmanatass <a.atassi2003@gmail.com>
2021-10-07 20:16:06 +0300

    fertured products

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import "./css/style.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "jquery/dist/jquery.min.js";
 import "popper.js/dist/popper.min.js";
-import "@laylazi/bootstrap-rtl/dist/css/bootstrap.min.css";
-console.log("TEST");
+import "@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css";
+import "@fortawesome/fontawesome-free/js/all.min";
+import "./css/style.css";
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
