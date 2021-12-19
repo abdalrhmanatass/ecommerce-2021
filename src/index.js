a909624130652c4971c9f090c8803466bae55687
abdalrhmanatass <a.atassi2003@gmail.com>
2021-12-19 13:06:49 +0200

    checkout page

--- src/index.js
+++ src/index.js
@@ -18,4 +18,51 @@ $(function () {
     $(this).parents(".product-option").siblings().removeClass("active");
     $(this).parents(".product-option").addClass("active");
   });
+
+  //when change the quntity
+  $("[data-product-quntity]").change(function () {
+    //
+    var newQuantity = $(this).val();
+
+    //
+    var $perent = $(this).parents("[data-product-info]");
+
+    //
+    var pricePerUnit = $perent.attr("data-product-price");
+
+    //
+    var totalPriceForProduct = newQuantity * pricePerUnit;
+
+    //
+    $perent.find(".total-price-for-product").text(totalPriceForProduct + "$");
+    calculateTotalPrice();
+  });
+
+  $("[data-remove-from-cart]").click(function () {
+    $(this).parents("[data-product-info]").remove();
+    calculateTotalPrice();
+  });
+
+  function calculateTotalPrice() {
+    //
+    var totalPriceForAllProduct = 0;
+
+    //
+    $("[data-product-info]").each(function () {
+      //
+      var pricePerUnit = $(this).attr("data-product-price");
+
+      //
+      var quntity = $(this).find("[data-product-quntity]").val();
+
+      //
+      var totalPriceForProduct = quntity * pricePerUnit;
+
+      //
+      totalPriceForAllProduct = totalPriceForAllProduct + totalPriceForProduct;
+    });
+
+    //
+    $("#total-price-for-all-products").text(totalPriceForAllProduct + "$");
+  }
 });
